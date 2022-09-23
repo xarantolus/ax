01c51156213ef39e817fd587e6213a38a8398e88
xarantolus <xarantolus@protonmail.com>
2022-09-23 21:15:51 +0200

    Minimal changes, but tests don't yet work

--- src/instructions/jrcxz.rs
+++ src/instructions/jrcxz.rs
@@ -35,7 +35,19 @@ impl Axecutor {
     fn instr_jrcxz_rel8_64(&mut self, i: Instruction) -> Result<(), AxError> {
         debug_assert_eq!(i.code(), Jrcxz_rel8_64);
 
-        todo!("instr_jrcxz_rel8_64 for Jrcxz")
+        if self.reg_read_64(SupportedRegister::RCX) == 0 {
+            match i.op0_kind() {
+                OpKind::NearBranch64 => {
+                    let offset = i.near_branch64() as i64 as u64;
+                    self.reg_write_64(SupportedRegister::RIP, offset);
+
+                    return Ok(());
+                }
+                _ => panic!("Invalid op0_kind for JMP rel32: {:?}", i.op0_kind()),
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -47,4 +59,27 @@ mod tests {
         write_reg_value,
     };
     use iced_x86::Register::*;
+
+    jmp_test![mov_rcx_0_jrcxz_end_mov_rcx_42_end_nop;
+        start: 0x401010; end: 0x401022;
+        0x48, 0xc7, 0xc1, 0x0, 0x0, 0x0, 0x0, 0xe3, 0x8; // mov rcx, 0; jrcxz .end
+        1; // 1 bytes of 0x90 (nop) as padding
+        0x48, 0xc7, 0xc1, 0x2a, 0x0, 0x0, 0x0, 0x90; // mov rcx, 42; .end: nop
+        |a: Axecutor| {
+            assert_reg_value!(q; a; RCX; 0);
+        };
+        (0; FLAG_CF | FLAG_PF | FLAG_ZF | FLAG_SF | FLAG_OF | FLAG_AF)
+    ];
+
+    jmp_test![mov_rax_0_mov_rcx_100_loop_jrcxz_end_add_rax_2_sub_rcx_1_jmp_loop_end_nop_pf_zf;
+        start: 0x401010; end: 0x40102c;
+        0x48, 0xc7, 0xc0, 0x0, 0x0, 0x0, 0x0, 0x48, 0xc7, 0xc1, 0x64, 0x0, 0x0, 0x0, 0xe3, 0xb; // mov rax, 0; mov rcx, 100; .loop: jrcxz .end
+        1; // 1 bytes of 0x90 (nop) as padding
+        0x48, 0x83, 0xc0, 0x2, 0x48, 0x83, 0xe9, 0x1, 0xeb, 0xf3, 0x90; // add rax, 2; sub rcx, 1; JMP .loop; .end: nop
+        |a: Axecutor| {
+            assert_reg_value!(q; a; RAX; 200);
+            assert_reg_value!(q; a; RCX; 0);
+        };
+        (FLAG_PF | FLAG_ZF; FLAG_CF | FLAG_SF | FLAG_OF | FLAG_AF)
+    ];
 }
