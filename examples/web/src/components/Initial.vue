ce5ad00af2bf0572bcd11c7b2be0c98a0ed3f937
xarantolus <xarantolus@protonmail.com>
2022-12-13 23:32:44 +0100

    Add proper terminal emulation on test site

--- examples/web/src/components/Initial.vue
+++ examples/web/src/components/Initial.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import { default as init, Axecutor, Mnemonic, Register } from 'ax';
-
-await init();
-</script>
-
 <template >
   <div class="middle width-2-3">
     <h1>AX Test Site</h1>
@@ -26,28 +20,57 @@ await init();
         <li><a href="/programs/hello_world.bin" @click.prevent="setBinary('hello_world.bin')">Hello World</a></li>
         <li><a href="/programs/alphabet.bin" @click.prevent="setBinary('alphabet.bin')">Alphabet</a></li>
         <li><a href="/programs/exit_code.bin" @click.prevent="setBinary('exit_code.bin')">Exit Code</a></li>
+        <li><a href="/programs/uppercase_naive.bin" @click.prevent="setBinary('uppercase_naive.bin')">Uppercase</a></li>
+        <li><a href="/programs/hex_naive.bin" @click.prevent="setBinary('hex_naive.bin')">Hex</a></li>
       </ul>
       You can also download these binaries and run them on Linux. This of course shows that the emulator can run some real binaries without any modifications.
     </div>
     <br />
     <h2>Console output</h2>
-    <pre id="console">{{ console_content }}</pre>
+    <Terminal ref="terminalRef" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
+import Terminal from './Terminal.vue';
+import { default as init, Axecutor, Mnemonic, Register } from 'ax';
 
 export default defineComponent({
-  data() {
-    return {
-      console_content: "This is the console output. It will contain stdout/stderr messages.",
+  components: {
+    Terminal,
+  },
+  async setup() {
+
+    const terminalRef = ref<InstanceType<typeof Terminal>>();
+
+    const termReset = () => {
+      terminalRef.value?.term.reset();
+      terminalRef.value?.term.clear();
+      terminalRef.value?.term.focus();
     };
+
+    onMounted(async () => {
+      // Download emulator
+      await init();
+
+      termReset();
+      terminalRef.value?.term.writeln('Welcome to the AX test site! When you run a binary, the output will be shown here.');
+    });
+
+    const termWrite = (data: string | Uint8Array) => {
+      terminalRef.value!.term.write(data);
+    };
+
+    return {
+      terminalRef,
+      termReset,
+      termWrite,
+    }
   },
   methods: {
     async syscallHandler(ax: Axecutor) {
       let syscall_num = ax.reg_read_64(Register.RAX);
-
       let rdi = ax.reg_read_64(Register.RDI);
       let rsi = ax.reg_read_64(Register.RSI);
       let rdx = ax.reg_read_64(Register.RDX);
@@ -56,21 +79,24 @@ export default defineComponent({
         case 0n: {
           // READ syscall MUST read from stdin
           if (rdi != 0n) {
-            throw new Error('READ syscall: cannot read from non-stdin (!= 0) fd, but tried ' + rdi);
+            throw new Error("READ syscall: cannot read from non-stdin (!= 0) fd, but tried " + rdi);
           }
 
           console.log("Reading " + rdx + " bytes from stdin");
 
-          let inputBytes;
+          let inputBytes = [];
           do {
-            let input = prompt("Please input " + rdx + " characters");
-            if (input == null) {
-              throw new Error('READ syscall: no input');
+            let byte = await this.terminalRef!.readByte();
+            if (byte === undefined) {
+              throw new Error("READ syscall: no input");
             }
-            inputBytes = new TextEncoder().encode(input);
-          } while (BigInt(inputBytes.length) != rdx);
+            inputBytes.push(byte);
+          } while (BigInt(inputBytes.length) < rdx);
 
-          ax.mem_write_bytes(rsi, inputBytes);
+          console.log(`Read ${inputBytes.length} bytes: ${inputBytes}, writing them to memory at ${rsi}`)
+
+          ax.mem_write_bytes(rsi, new Uint8Array(inputBytes));
+          ax.reg_write_64(Register.RAX, BigInt(inputBytes.length));
 
           return ax.commit();
         }
@@ -82,106 +108,100 @@ export default defineComponent({
           }
 
           let result_buf = ax.mem_read_bytes(rsi, rdx);
-          let result_str = new TextDecoder().decode(result_buf);
 
-          console.log("Writing " + rdx + " bytes to stdout/stderr: " + result_str);
-
-          this.console_content += result_str;
+          this.termWrite(result_buf);
 
           return ax.unchanged();
         }
         case 60n: {
           // EXIT syscall
-          console.log("Exiting with code " + rdi);
           return ax.stop();
         }
         default: {
-          throw new Error('Syscall: unsupported RAX value ' + syscall_num);
+          throw new Error("Syscall: unsupported RAX value " + syscall_num);
         }
       }
     },
     async setBinary(name: string) {
       try {
-        this.console_content = `Downloading ${name}...\n`;
-        let response = await fetch(`/programs/${name}`);
-        let content = await response.arrayBuffer().then(buf => new Uint8Array(buf));
+        this.termReset();
 
+        this.termWrite(`Downloading ${name}...\n`);
+
+        let response = await fetch(`/programs/${name}`);
+
+        if (!response.ok) {
+          this.termWrite("Error while fetching binary: " + response.statusText);
+          return;
+        }
+
+        let content = await response.arrayBuffer().then(buf => new Uint8Array(buf));
         // Create a new file object from the binary and set it as the file input's value
         let file = new File([content], name, { type: "application/octet-stream" });
-
         let data = new DataTransfer();
         data.items.add(file);
-
         (this.$refs.file as any).files = data.files;
 
-        this.console_content += `Successfully downloaded ${name}, you can now run it.\n`;
-      } catch (e) {
-        this.console_content = "Error while fetching binary:\n" + e;
+        this.termWrite(`Successfully downloaded ${name}, you can now run it.\n`);
+      }
+      catch (e) {
+        this.termWrite("Error while fetching binary:\n" + e);
         return;
       }
     },
     async runFile() {
-      this.console_content = "";
+      this.termReset();
 
       let ax;
-
       try {
         let files = (this.$refs.file as any).files as FileList;
         console.log(files);
-
         if (files.length != 1) {
-          alert("Please select exactly one file");
+          this.termWrite("Please select exactly one file");
           return;
         }
-
         let content = await files.item(0)?.arrayBuffer().then(buf => new Uint8Array(buf));
         if (!content) {
-          alert("Failed to read file, please only use statically linked ELF binaries.");
+          this.termWrite("Failed to read file, please only use statically linked ELF binaries.");
           return;
         }
-
         ax = Axecutor.from_binary(content);
-      } catch (e) {
+      }
+      catch (e) {
         let axstate = (ax ?? "no axecutor state available").toString();
-        console.error(axstate)
-        this.console_content = "Error while decoding binary:\n" + e;
+        console.error(axstate);
+        this.termWrite("Error while decoding binary:\n" + e);
         return;
       }
-
       try {
         ax.init_stack(8n * 1024n);
-
         let rsp = ax.reg_read_64(Register.RSP);
         ax.reg_write_64(Register.RSP, rsp - 1024n);
-
         ax.hook_before_mnemonic(Mnemonic.Ret, (ax: Axecutor) => {
           console.log("before RET @ " + ax.reg_read_64(Register.RIP));
-
           return ax.unchanged();
         });
-
         ax.hook_after_mnemonic(Mnemonic.Ret, (ax: Axecutor) => {
           console.log("after RET @ " + ax.reg_read_64(Register.RIP));
-
           return ax.unchanged();
         });
-
         ax.hook_before_mnemonic(Mnemonic.Syscall, this.syscallHandler);
-      } catch (e) {
+      }
+      catch (e) {
         let axstate = (ax ?? "no axecutor state available").toString();
-        console.error(axstate)
-        this.console_content += "\nError during initialisation:\n" + e + "\n\n" + axstate;
+        console.error(axstate);
+        this.termWrite("Error during initialisation:\n" + e + "\n\n" + axstate);
         return;
       }
-
       try {
+        this.termWrite("Starting emulation...\n");
         await ax.execute();
-
-        this.console_content += `Program exited with exit code ${ax.reg_read_64(Register.RDI)}.`;
-      } catch (e) {
+        this.termWrite(`Program exited with exit code ${ax.reg_read_64(Register.RDI)}.\n`);
+      }
+      catch (e) {
         let axstate = (ax ?? "no axecutor state available").toString();
-        console.error(axstate)
-        this.console_content += "\nError during execution:\n" + e + "\n\n" + axstate;
+        console.error(axstate);
+        this.termWrite("\nError during execution:\n" + e + "\n\n" + axstate);
       }
     }
   }
@@ -191,5 +211,11 @@ export default defineComponent({
 <style>
 .width-2-3 {
   width: 66.67%;
+}
+
+@media (max-width: 1440px) {
+  .width-2-3 {
+    width: 100%;
+  }
 }
 </style>
---
+++ examples/web/src/components/Terminal.vue
@@ -0,0 +1,137 @@
+<template>
+	<div class="terminal" ref="terminalHTMLElement"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, onUnmounted, ref, type Ref } from 'vue';
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
+import 'xterm/css/xterm.css';
+
+export default defineComponent({
+	name: 'Terminal',
+	setup(props, context) {
+		// Setup xterm with automatic resize
+		const terminalHTMLElement: Ref<HTMLElement | null> = ref(null);
+		const term: Terminal = new Terminal({
+			convertEol: true,
+		});
+		const fitAddon: FitAddon = new FitAddon();
+		term.loadAddon(fitAddon);
+
+		let resizeHandler = () => {
+			fitAddon.fit();
+		};
+
+		let termBuffer = new Uint8Array();
+		let inputBufferEventElement = document.createElement("div");
+		let inputAllowed = false;
+
+		let mergeBuffers = (arrayOne: Uint8Array, arrayTwo: Uint8Array) => {
+			if (arrayOne.length === 0) return arrayTwo;
+			let mergedArray = new Uint8Array(arrayOne.length + arrayTwo.length);
+			mergedArray.set(arrayOne);
+			mergedArray.set(arrayTwo, arrayOne.length);
+			return mergedArray;
+		};
+
+		let keyInputHandler = (input: { key: string; domEvent: KeyboardEvent; }, _: void) => {
+			// Length is 1 for printable characters, e.g. "a", "ö" etc; however not for special keys
+			if (!inputAllowed || input.key.length !== 1) return;
+
+
+			let inputBytes = new TextEncoder().encode(input.key.replace("\r", "\n"));
+			if (inputBytes.length === 0) return;
+
+			// Echo back what was put in
+			term.write(inputBytes);
+
+			termBuffer = mergeBuffers(termBuffer, inputBytes);
+
+			if (termBuffer.length === 0) return;
+
+			const event = new Event('buffer');
+			inputBufferEventElement.dispatchEvent(event);
+		}
+
+		onMounted(() => {
+			if (!terminalHTMLElement.value) throw new Error("Terminal HTML element not found");
+
+			term.open(terminalHTMLElement.value);
+			window.addEventListener('resize', resizeHandler);
+			resizeHandler();
+
+			term.onKey(keyInputHandler);
+		});
+
+		let readByte = () => {
+			let readFirstBufferByte = () => {
+				let byte = termBuffer[0];
+				termBuffer = termBuffer.slice(1);
+				return Promise.resolve(byte);
+			}
+
+			console.log("readByte", termBuffer);
+			if (termBuffer.length > 0) {
+				console.log("reading from buffer");
+				return readFirstBufferByte();
+			}
+
+			let setInputAllowed = (allowed: boolean) => {
+				inputAllowed = allowed;
+				term.options.cursorBlink = allowed;
+			}
+
+			console.log("waiting for input");
+			setInputAllowed(true);
+
+			// The next time something is put in the buffer we return it
+			return new Promise<number>((resolve) => {
+				inputBufferEventElement.addEventListener('buffer', async function () {
+					setInputAllowed(false);
+					resolve(readFirstBufferByte());
+				}, {
+					once: true,
+				});
+			})
+		}
+
+		context.expose({
+			term,
+			readByte,
+		});
+
+		onUnmounted(() => {
+			window.removeEventListener('resize', resizeHandler);
+			term.dispose();
+		});
+
+		return {
+			terminalHTMLElement,
+			term,
+			readByte,
+		};
+	},
+})
+
+</script>
+
+<style scoped>
+.terminal {
+	text-align: left;
+	width: 100%;
+	height: 100%;
+}
+</style>
+
+<style>
+.terminal-container {
+	/* this is important */
+	overflow: hidden;
+}
+
+.xterm .xterm-viewport {
+	/* see : https://github.com/xtermjs/xterm.js/issues/3564#issuecomment-1004417440 */
+	width: initial !important;
+}
+</style>
