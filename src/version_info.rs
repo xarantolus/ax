ab1e31fbcdbf7e76799636602834468fe4be8a36
xarantolus <xarantolus@protonmail.com>
2023-01-02 11:20:43 +0100

    Remove unnecessary test

--- src/version_info.rs
+++ src/version_info.rs
@@ -6,13 +6,3 @@ pub fn version() -> String {
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
     VERSION.to_string()
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_semantic_version_pattern() {
-        assert!(version().matches(r"^\d+\.\d+\.\d+$").count() == 1);
-    }
-}
