2d6cc3650d5bf6582b994a0bf1cfad10f2af0ac9
xarantolus <xarantolus@protonmail.com>
2023-01-02 11:19:33 +0100

    Don't warn of unused version function

--- src/version_info.rs
+++ src/version_info.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
+#[allow(dead_code)]
 pub fn version() -> String {
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
     VERSION.to_string()
