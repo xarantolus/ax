c55c8d5de9dbf42266623e870655b4795332154a
xarantolus <xarantolus@protonmail.com>
2023-01-05 11:05:19 +0100

    Improve build script

--- src/build.rs
+++ src/build.rs
@@ -1,11 +1,10 @@
 use std::process::Command;
 
 fn main() {
-    // note: add error checking yourself.
     let output = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+        .expect("Failed to execute git rev-parse HEAD.");
+    let git_hash = String::from_utf8(output.stdout).unwrap().trim().to_string();
     println!("cargo:rustc-env=GIT_COMMIT={}", git_hash);
 }
