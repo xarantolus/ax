0a680ef576735d3be1a573b9e8b0d31ecf2a51ce
xarantolus <xarantolus@protonmail.com>
2023-01-26 10:20:25 +0100

    Use different ELF parser

--- src/elf/mod.rs
+++ src/elf/mod.rs
@@ -1,18 +1,16 @@
-extern crate elf_rs;
-use elf_rs::*;
+extern crate elf;
+use elf::endian::AnyEndian;
+use elf::{ElfBytes, ParseError};
+
 use std::string::FromUtf8Error;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 use crate::helpers::debug::debug_log;
 use crate::{axecutor::Axecutor, helpers::errors::AxError};
 
-impl From<Error> for AxError {
-    fn from(err: Error) -> Self {
-        match err {
-            Error::BufferTooShort => AxError::from("ELF: Buffer too short"),
-            Error::InvalidMagic => AxError::from("ELF: Invalid magic"),
-            Error::InvalidClass => AxError::from("ELF: Invalid class"),
-        }
+impl From<ParseError> for AxError {
+    fn from(err: ParseError) -> Self {
+        AxError::from(format!("ELF: Parse error: {}", err))
     }
 }
 
@@ -30,68 +28,89 @@ impl Axecutor {
     pub fn from_binary(binary: &[u8]) -> Result<Axecutor, AxError> {
         debug_log!("Calling Axecutor::from_binary");
 
-        let obj_file = Elf::from_bytes(binary)?;
+        let file = ElfBytes::<AnyEndian>::minimal_parse(binary)?;
 
-        let text_section = match obj_file.lookup_section(b".text") {
+        let text_section = match file.section_header_by_name(".text")? {
             Some(section) => section,
             None => return Err(AxError::from("ELF: No .text section")),
         };
-        let text_section_content = match text_section.content() {
-            Some(content) => content,
-            None => return Err(AxError::from("ELF: .text section has no content")),
+
+        let text_section_content = match file.section_data(&text_section)? {
+            (data, None) => data,
+            (_, Some(_)) => {
+                return Err(AxError::from("ELF: Compressed .text section not supported"))
+            }
         };
 
+        let entrypoint = file.ehdr.e_entry;
         let mut axecutor = Axecutor::new(
             text_section_content,
-            text_section.addr(),
-            match obj_file.entry_point() {
-                0 => text_section.addr(),
-                n => n,
+            text_section.sh_addr,
+            if entrypoint == 0 {
+                text_section.sh_addr
+            } else {
+                entrypoint
             },
         )?;
 
-        // See https://docs.oracle.com/cd/E19683-01/816-1386/chapter6-83432/index.html
-        for (i, header) in obj_file.program_header_iter().enumerate() {
-            let content = match header.content() {
-                Some(c) => c,
-                None => continue,
-            };
-            if header.vaddr() == text_section.addr() {
+        let headers = match file.segments() {
+            Some(headers) => headers,
+            None => return Err(AxError::from("ELF: No program headers")),
+        };
+
+        for header in headers {
+            if header.p_vaddr == text_section.sh_addr {
                 // skip .text section -- we already loaded it
                 // Aspirationally this should go away once the memory implementation also holds the code
                 continue;
             }
 
-            match header.ph_type() {
-                ProgramType::LOAD => {
-                    if header.memsz() > header.filesz() {
+            let content = file.segment_data(&header)?;
+
+            match header.p_type {
+                0 => continue, // ignored
+                1 => {
+                    // LOAD
+
+                    debug_log!(
+                        "ELF: Loading header at {:#x} with size {:#x} and offset {:#x}",
+                        header.p_vaddr,
+                        header.p_memsz,
+                        header.p_offset
+                    );
+
+                    if header.p_memsz == header.p_filesz {
+                        axecutor.mem_init_area_named(
+                            header.p_vaddr,
+                            content.to_vec(),
+                            Some(format!("elf_header_{:#x}", header.p_vaddr)),
+                        )?;
+                    } else {
                         // Make sure we create the memory at full size and then write the first bytes, rest should be zeroed
                         axecutor.mem_init_zero_named(
-                            header.vaddr(),
-                            header.memsz(),
-                            format!("elf_zeroed_header{}", i),
+                            header.p_vaddr,
+                            header.p_memsz,
+                            format!("elf_zeroed_header_{:#x}", header.p_vaddr),
                         )?;
 
-                        if content.len() > header.memsz() as usize {
+                        if content.len() > header.p_filesz as usize {
                             return Err(AxError::from(
-                                "ELF: content of program header is longer than headers' memsz"
-                                    .to_string(),
+                                "ELF: Content is larger than specified in header".to_string(),
                             ));
                         }
 
-                        axecutor.mem_write_bytes(header.vaddr(), content)?;
-                    } else {
-                        axecutor.mem_init_area_named(
-                            header.vaddr(),
-                            content.to_vec(),
-                            Some(format!("elf_header{}", i)),
+                        axecutor.mem_write_bytes(
+                            header.p_vaddr,
+                            &content[..header.p_filesz as usize],
                         )?;
                     }
                 }
-                ProgramType::DYNAMIC => {
-                    return Err(AxError::from("ELF: DYNAMIC program header not supported"))
+                2 => {
+                    return Err(AxError::from("ELF: Dynamic linking not supported"));
                 }
-                _ => continue, // ignored
+                v => {
+                    debug_log!("ELF: Ignoring section type {:#x}", v);
+                }
             }
         }
 
