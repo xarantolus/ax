c506a5cb7cd1ecdb4e49f9870d0b47e04d3b2eb6
xarantolus <xarantolus@protonmail.com>
2023-04-06 11:30:46 +0200

    Improve debug log spacing

--- src/helpers/debug.rs
+++ src/helpers/debug.rs
@@ -8,27 +8,29 @@ extern "C" {
 
 macro_rules! debug_log {
     ($str:expr) => {
+        let p = &*format!("{}:{}: ", file!(), line!());
         #[cfg(all(target_arch = "wasm32", debug_assertions, not(test)))]
         {
             use $crate::helpers::debug::js_debug_log;
-            js_debug_log(&*format!("{}:{}: {}", file!(), line!(), $str));
+            js_debug_log(&*format!("{: <30}{}", p, $str));
         }
 
         #[cfg(all(not(target_arch = "wasm32"), debug_assertions))]
         {
-            println!("{}:{}: {}", file!(), line!(), $str);
+            println!("{: <30}{}", p, $str);
         }
     };
     ($fmt:expr, $($arg:tt)*) => {
+        let p = &*format!("{}:{}: ", file!(), line!());
         #[cfg(all(target_arch = "wasm32", debug_assertions, not(test)))]
         {
             use $crate::helpers::debug::js_debug_log;
-            js_debug_log(&*format!("{}:{}: {}", file!(), line!(), format!($fmt, $($arg)*)));
+            js_debug_log(&*format!("{: <30}{}", p, format!($fmt, $($arg)*)));
         }
 
         #[cfg(all(not(target_arch = "wasm32"), debug_assertions))]
         {
-            println!("{}:{}: {}", file!(), line!(), format!($fmt, $($arg)*));
+            println!("{: <30}{}", p, format!($fmt, $($arg)*));
         }
     };
 }
